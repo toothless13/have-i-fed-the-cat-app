d812172564f6a708bd9091649a390086c3e47238
Tim <50870607+toothless13@users.noreply.github.com>
2023-09-06 12:21:15 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.use(express.json());
+
+app.post('/cats', (req, res) => {
+  res.status(201).json(req.body);
+});
+
+module.exports = app;
